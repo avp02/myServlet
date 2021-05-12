989b7558139da2c71cceb96a4dc6a4ab8a13e8f4
VladimirPetranovski <desantluninec@mail.ru>
2021-05-12 10:53:45 +0300

    changed web.xml, added StudentCard.java

---
+++ students/src/main/java/com/avp/myservlets/StudentCard.java
@@ -0,0 +1,44 @@
+package com.avp.myservlets;
+
+import com.avp.bean.Student;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class StudentCard extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = 0;
+        if(Student.total.size()==0) {
+            Student.total.add(new Student(0L, "Maksim", "Shelkovich",
+                    "my@email", 35));
+        }
+        Student student = Student.total.get(id);
+
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                " <title>timer</title>\n" +
+                "</head>\n" +
+                "<body align=\"center\">\n" +
+                "<h1> This one will never change! </h1>\n" +
+                "<table align=\"center\">\n" +
+                " <tr>\n" +
+                " <td>\n" +
+                " <img src=\"https://upload.wikimedia.org /wikipedia/commons/a/a0/Reuni%C3%A3o_com_o_ator_norteamericano_Keanu_Reeves_%28cropped%29.jpg\"\n" +
+                " width=\"50px\"\n" +
+                " height=\"50px\"/>\n" +
+                " </td>\n" +
+                " <td>"+student.getName() + " " + student.getLastName() + " <br />" + student.getEmail() +"</td>\n" +
+        " </tr>\n" +
+                "</table>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+}
